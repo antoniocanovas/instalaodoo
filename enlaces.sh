fa51001674f72696a4d11c520c23b24ad511784b
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2019-03-13 18:50:26 +0100

    Primera versión v12

---
+++ enlaces.sh
@@ -0,0 +1,16 @@
+ln -s ../community-data-files/base_bank_from_iban ./
+ln -s ../server-ux/date_range ./
+ln -s ../CybroAddons/developer_mode ./
+ln -s ../server-ux/mass_editing ./
+ln -s ../mis-builder/mis_builder ./
+ln -s ../muk_base/muk_utils ./
+ln -s ../muk_web/muk_web_theme ./
+ln -s ../muk_web/muk_web_utils ./
+ln -s ../reporting-engine/report_xlsx ./
+ln -s ../web/web_responsive ./
+ln -s ../web/web_widget_color ./
+# Para el SII:
+ln -s ../account-invoicing/account_invoice_refund_link ./
+ln -s ../account-financial-reporting/account_tax_balance ./
+ln -s ../queaue/queue_job
+
