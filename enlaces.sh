cf31ea341916f274ac55714392c056e704b7a180
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2019-03-14 16:48:29 +0100

    enlaces con presupuestaria mis

--- enlaces.sh
+++ enlaces.sh
@@ -2,16 +2,19 @@ ln -s ../community-data-files/base_bank_from_iban ./
 ln -s ../server-ux/date_range ./
 ln -s ../CybroAddons/developer_mode ./
 ln -s ../server-ux/mass_editing ./
-ln -s ../mis-builder/mis_builder ./
-ln -s ../muk_base/muk_utils ./
-ln -s ../muk_web/muk_web_theme ./
-ln -s ../muk_web/muk_web_utils ./
-ln -s ../reporting-engine/report_xlsx ./
-ln -s ../web/web_responsive ./
-ln -s ../web/web_widget_color ./
 # Para el SII:
 ln -s ../account-invoicing/account_invoice_refund_link ./
 ln -s ../account-financial-reporting/account_tax_balance ./
 ln -s ../queue/queue_job
 # Para copias:
 ln -s ../auto_backup/auto_backup ./
+# Para informes mis:
+ln -s ../mis-builder/mis_builder_budget/ ./
+ln -s ../mis-builder/mis_builder ./
+ln -s ../reporting-engine/report_xlsx ./
+ln -s ../web/web_widget_color ./
+# Bakcend:
+ln -s ../muk_web/muk_web_utils ./
+ln -s ../web/web_responsive ./
+ln -s ../muk_web/muk_web_theme ./
+ln -s ../muk_base/muk_utils ./
