f4609d3bf846e0bb22c83de10617b8dc40ad5ff4
Antonio Cánovas <antonio.canovas@ingenieriacloud.com>
2022-06-17 13:12:08 +0200

    Update update_all.sh

--- update_all.sh
+++ update_all.sh
@@ -6,9 +6,9 @@
 ################################################
 
 # add to log all to a file
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>log-update-all.out 2>&1
+#exec 3>&1 4>&2
+#trap 'exec 2>&4 1>&3' 0 1 2 3
+#exec 1>log-update-all.out 2>&1
 # Everything below will go to the file 'log.out':
 
 if [ "$(id -u)" != "0" ]; then
@@ -21,7 +21,7 @@ RUTA=`pwd`
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     /etc/init.d/odoo-$line stop
-    su odoo -c "/opt/odoo/ocb/odoo-bin -c /etc/odoo/odoo-$line.conf -d $line -u all --stop-after-init --workers=0"
+    su odoo -c "/opt/odoo/odoo/odoo-bin -c /etc/odoo/odoo-$line.conf -d $line -u all --stop-after-init --workers=0"
     /etc/init.d/odoo-$line start
 
 done < $RUTA/instancias.txt
