c242a9c2579b31d93a59e4bf4062ffd17d56ac70
Antonio Cánovas <antonio.canovas@ingenieriacloud.com>
2022-11-05 20:15:03 +0100

    rev nov 12

--- enlaces14.sh
+++ enlaces14.sh
@@ -8,9 +8,12 @@ ln -s ../server-ux/mass_operation_abstract/ ./
 ln -s ../server-auth/password_security ./
 ln -s ../server-auth/auth_admin_passkey ./
 # Contabilidad:
-ln -s ../account-financial-tools/account_chart_update/ ./
-ln -s ../account-financial-reporting/account_financial_report/ ./
-ln -s ../account-financial-tools/account_asset_management/ ./
+ln -s ../account-financial-tools/account_chart_update ./
+ln -s ../account-financial-reporting/account_financial_report ./
+ln -s ../account-financial-tools/account_asset_management ./
+ln -s ../bank-statement-import/account_statement_import_base ./
+ln -s ../bank-statement-import/account_statement_import ./
+ln -s ../account-reconcile/account_reconciliation_widget ./
   # Wizard bloquear fechas:
 ln -s ../account-financial-tools/account_lock_date_update ./
   # Crear años fiscales:
@@ -24,30 +27,23 @@ ln -s ../server-tools/auto_backup ./
 # Para bancos:
 #ln -s ../bank-statement-import/account_bank_statement_import_txt_xlsx/
 ln -s ../web/web_widget_dropdown_dynamic ./
-ln -s ../server-ux/multi_step_wizard/ ./
+ln -s ../server-ux/multi_step_wizard ./
 # Para informes mis:
-ln -s ../mis-builder/mis_builder_budget/ ./
+ln -s ../mis-builder/mis_builder_budget ./
 ln -s ../mis-builder/mis_builder ./
 ln -s ../reporting-engine/report_xlsx ./
-#ln -s ../web/web_widget_color ./
 # Backend:
-#ln -s ../muk_web/muk_web_utils ./
 ln -s ../web/web_responsive ./
 ln -s ../muk_web/muk_web_theme ./
-#ln -s ../muk_base/muk_utils ./
-#ln -s ../muk_base/muk_autovacuum/ ./
 # Cheques y otros contables:
 ln -s ../account-financial-tools/account_check_deposit/ ./
-#ln -s ../account-financial-tools/account_renumber/ ./
 # GRPD:
 ln -s ../data-protection/privacy ./
 # MULTICOMPANY:
 ln -s ../multi-company/base_multi_company ./
 ln -s ../multi-company/account_multicompany_easy_creation ./
-#ln -s ../multi-company/res_company_active ./
 # Otros varios:
 ln -s ../social/mail_debrand ./
-#ln -s ../addons-ic/user_manager_permission_limit ./
 # Para asesorías:
 ln -s ../addons-ic/odoo_sso_client ./
 # SEPA:
